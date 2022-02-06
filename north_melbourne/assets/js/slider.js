37b92cadee24b49165dec9dc5b4e1599a9581b2a
kiki-maulana <65699136+kiki-maulana@users.noreply.github.com>
2022-02-07 09:48:01 +1100

    init north_melbourne

---
+++ north_melbourne/assets/js/slider.js
@@ -0,0 +1,50 @@
+slider('range-2');
+function slider(s_id) {
+
+    // assemble the tag id's we'll need later
+    var range       = '#'+s_id;
+    var inputRange  = '#input-'+s_id;
+    var labelsRange = '#labels-'+s_id;
+
+    var getTrackStyle = function (elem) {  
+        var curVal = elem.value;
+        // The number of stops determines the multiplier
+        // below. For example, if there are 7 stops in
+        // total then there will be 6 blank spaces between
+        // them. So if 100% is the total length then:
+        //      100 / 6 = 16.666666667% 
+        var step = 100 / ($(inputRange).attr('max') - 1);
+        // 
+        var seltrack = (curVal - 1) * step;
+        
+        // Set active label
+        $(labelsRange+' li').removeClass('active selected');
+        var curLabel = $(labelsRange).find('li:nth-child(' + curVal + ')');
+        curLabel.addClass('active selected');
+        curLabel.prevAll().addClass('selected');
+
+        // console.log('getTrackStyle - '+labelsRange+' = '+curLabel.data('stop'));
+        var style = range+' {background: linear-gradient(to right, var(--slider-active-color) 0%, var(--slider-active-color) ' + seltrack + '%, '+$(range).parent().css('background-color')+' ' + seltrack + '%, '+$(range).parent().css('background-color')+' 100%);}\n';
+        // console.log('getTrackStyle - style = ['+style+']');
+        return style;
+    };
+
+    var sheet = document.createElement('style');
+    document.body.appendChild(sheet);
+    
+    // when a stop is selected apply the necessary
+    // styling and get the usable value of the stop.
+    $(inputRange).on('input', function () {
+        sheet.textContent = getTrackStyle(this);
+        let dataKML = $('#labels-range-2').find('.active.selected').data('kml')
+        let dataKML2 = $('#labels-range-2').find('.active.selected').data('kml2')
+        fechKML('map', dataKML, beforeMap)
+        fechKML('map2', dataKML2, beforeMap2)
+    });
+
+    // Change input value on label click
+    $(labelsRange+' li').on('click', function () {
+        var index = $(this).index();
+        $(inputRange).val(index + 1).trigger('input');
+    });
+};
---
+++ north_melbourne/content.md
@@ -0,0 +1,26 @@
+# Why city needs to grow, and it's not CBD
+
+#
+## Northern Melbourne, from Past to the Future
+
+Melbourne is the capital city of Victoria with the estimated total population around 5 million people. 
+The city was founded on 1835 on the Yarra River. Due to the popularity as one of the most livable city in the world, the city is attracting many people around the world to move, stay and live in Melbourne. 
+To accomodate the rapid population grows, the city is expanding to all directions together with all supportive infrastructures.
+
+Road one of the most important infrastructures that connect all important places together, which allows the city live. 
+All of the city aspects need a good and reliable road infrastructure, which in turn support the society, economic, logistics, and all other activities to work. 
+Furthermore, the road also support the urban development to be done. The road is the first infrastucture that need to be provided before the urban development can be done. 
+The road also attracts investors to invest their businesses in a new area, and attracts people to move to a new area. 
+Since the road infrastructure is the key to the city development, the road development will reflect how the city growed from time to time. 
+
+The City of Hume is located on the northern part of Melbourne. 
+This  munacipality is considered as the northnern gateway of meltropolitan Melbourne where the interstate M31-Hume Freeway is located. 
+Back in the day where this freeway was not there, the Sydney road (also known as Hume Highway) was a major arterial road for the interstate travellers. 
+The Hume Historical Map will bring the viewer back in time in 1966 to observe the development of road infrastructure in one of the important municipality on the northern part of metropolitan Melbourne. 
+The audience will have capability to check the infrastrcture development from one period of time to another. 
+
+For further inquiry, contact [Assoc. Prof. David Taniar](mailto:david.taniar@monash.edu).
+
+
+
+Created by [k](mailto:k.adhinugraha@latrobe.edu.au) | [PondokIT](https://pondokit.com) | 2022
